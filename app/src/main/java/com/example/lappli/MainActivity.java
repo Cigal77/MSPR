acd3376c5a300c148c15ef004cdc3b917c360451
Anthony <anthonydr@free.fr>
2021-03-11 09:22:43 +0100

    update

--- app/src/main/java/com/example/lappli/MainActivity.java
+++ app/src/main/java/com/example/lappli/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.lappli;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button qrbtn;
+    TextView qrtext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        qrbtn=(Button)findViewById(R.id.qrbtn);
+        qrtext=(TextView)findViewById(R.id.qrtext);
+
+                qrbtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
     }
 }
