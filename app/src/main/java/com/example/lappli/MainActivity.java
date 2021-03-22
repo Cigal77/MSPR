f86e44450479dbac544c8c6e567fba652838ee33
Anthony <anthonydr@free.fr>
2021-03-22 13:15:57 +0100

    scan ok

--- app/src/main/java/com/example/lappli/MainActivity.java
+++ app/src/main/java/com/example/lappli/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lappli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     Button qrbtn;
-    TextView qrtext;
+    public static TextView qrtext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
                 qrbtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-
+                        startActivity(new Intent(getApplicationContext(),qrscanner.class));
                     }
                 });
     }
--- app/src/main/java/com/example/lappli/qrscanner.java
+++ app/src/main/java/com/example/lappli/qrscanner.java
@@ -1,10 +1,15 @@
 package com.example.lappli;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.zxing.Result;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -18,12 +23,15 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 public class qrscanner extends AppCompatActivity implements ZXingScannerView.ResultHandler
 {
     ZXingScannerView scannerView;
+    DatabaseReference dbref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         scannerView= new ZXingScannerView(this);
         setContentView(scannerView);
+        dbref= FirebaseDatabase.getInstance().getReference("qrscannerdata");
 
         Dexter.withContext(getApplicationContext())
                 .withPermission(Manifest.permission.CAMERA)
@@ -48,6 +56,15 @@ public class qrscanner extends AppCompatActivity implements ZXingScannerView.Res
     @Override
     public void handleResult(Result rawResult) {
         String data = rawResult.getText().toString();
+
+            dbref.push().setValue(data)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            MainActivity.qrtext.setText("Data inserted Successfully");
+                            onBackPressed();
+                        }
+                    });
     }
 
     @Override
